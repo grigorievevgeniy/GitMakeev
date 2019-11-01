40ff2fea83c948b2913ce923284b2f3a609a3cf0
Евгений Григорьев <evgeniy.grigoryev@simbirsoft.com>
2019-11-01 17:12:08 +0300

    #4 Добавлен класс города

---
+++ City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitMakeev
+{
+    class City
+    {
+        public int Popular { get; set; }
+    }
+}
